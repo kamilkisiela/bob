5dac2fac288f94f8b599c4de3daa3e80e7f64950
Arda TANRIKULU <ardatanrikulu@gmail.com>
2019-11-27 12:05:06 -0500

    Take deps from src package.json

--- src/update-version.ts
+++ src/update-version.ts
@@ -12,4 +12,17 @@ const srcPackageJson = readPackageJson(cwd);
 const distPackageJson = readPackageJson(join(cwd, './dist'));
 
 distPackageJson.version = srcPackageJson.version;
+if ('dependencies' in srcPackageJson) {
+    distPackageJson.dependencies = srcPackageJson.dependencies;
+}
+if ('peerDependencies' in srcPackageJson) {
+    distPackageJson.peerDependencies = srcPackageJson.peerDependencies;
+}
+if ('optionalDependencies' in srcPackageJson) {
+    distPackageJson.optionalDependencies = srcPackageJson.optionalDependencies;
+}
+if ('sideEffects' in srcPackageJson) {
+    distPackageJson.sideEffects = srcPackageJson.sideEffects;
+}
+
 writeFileSync(join(cwd, './dist/package.json'), JSON.stringify(distPackageJson, null, 2));
