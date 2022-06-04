635e27bd2ac33f3ea124b5f7b9aeee172bb73ade
Laurin Quast <laurinquast@googlemail.com>
2022-06-04 07:59:17 -0700

    feat: **BREAKING** require TypeScript module resolution compatible exports map

--- src/utils/rewrite-exports.ts
+++ src/utils/rewrite-exports.ts
@@ -1,5 +1,5 @@
 export function rewriteExports(
-  exports: Record<string, string | { require?: string; import?: string }>,
+  exports: Record<string, string | { require?: string | { [key: string]: string }; import?: string | { [key: string]: string } }>,
   distDir: string
 ) {
   const newExports = { ...exports };
@@ -7,29 +7,55 @@ export function rewriteExports(
   newExports["./package.json"] = "./package.json";
 
   newExports["."] = {
-    require: "./index.js",
-    import: "./index.mjs",
+    require: {
+      default: "./index.js",
+      types: "./index.d.ts"
+    },
+    import: {
+      default: "./index.mjs",
+      types: "./index.d.ts",
+    },
   };
 
   newExports["./*"] = {
-    require: "./*.js",
-    import: "./*.mjs",
+    require:  {
+      default: "./*.js",
+      types: "./*.d.ts"
+    },
+    import: {
+      default: "./*.mjs",
+      types: "./*.d.ts"
+    },
   };
 
   for (const [key, value] of Object.entries(newExports)) {
     if (!value) continue;
 
-    let newValue = value as string | { require?: string; import?: string };
+    let newValue = value as string | { require?: string | { [key: string]: string }; import?: string | { [key: string]: string } };
 
     if (typeof newValue === "string") {
       newValue = newValue.replace(`${distDir}/`, "");
     } else if (typeof newValue === "object" && newValue != null) {
+
+      function transformValue(value: string | { [key: string]: string } | undefined) {
+        if (value == null) {
+          return undefined
+        }
+        if (typeof value === "object") {
+          const newValue: Record<string, string> = {}
+          for (const [key, path] of Object.entries(value)) {
+            newValue[key] = path.replace(`${distDir}/`, "")
+          }
+          return newValue
+        }
+        return value.replace(`${distDir}/`, "")
+      }
+
       newValue = {
-        require: newValue.require?.replace(`${distDir}/`, ""),
-        import: newValue.import?.replace(`${distDir}/`, ""),
+        require: transformValue(newValue.require),
+        import: transformValue(newValue.import),
       };
     }
-
     newExports[key.replace(`${distDir}/`, "")] = newValue;
   }
 
