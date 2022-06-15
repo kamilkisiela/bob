39582a888ab4d72a57507ec0f33f0ffe5c66bbfd
Laurin Quast <laurinquast@googlemail.com>
2022-06-15 12:35:21 +0200

    chore: remove the github action stuff which is no longer used

--- action/rollup-plugins/auto-external.d.ts
+++
@@ -1,7 +0,0 @@
-import type { Plugin } from "rollup";
-declare type Options = {
-    packageJSONPath: string;
-};
-export declare function autoExternal({ packageJSONPath }: Options): Plugin;
-export {};
-//# sourceMappingURL=auto-external.d.ts.map
