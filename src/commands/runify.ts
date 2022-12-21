83d9e332d0ca24ccd8f2a3aa42eab7652cf92a08
Kamil Kisiela <kamil.kisiela@gmail.com>
2022-12-21 16:09:55 +0100

    Use Node18 as target in runfiy

--- src/commands/runify.ts
+++ src/commands/runify.ts
@@ -211,7 +211,7 @@ async function buildNext(cwd: string, additionalRequire: string | null) {
     await tsup({
       entryPoints: [join(cwd, additionalRequire)],
       outDir: join(cwd, "dist"),
-      target: "node16",
+      target: "node18",
       format: ["cjs"],
       splitting: false,
       skipNodeModulesBundle: true,
@@ -268,7 +268,7 @@ async function compile(
     await tsup({
       entryPoints: [join(cwd, entryPoint)],
       outDir: out,
-      target: "node16",
+      target: "node18",
       format: [useEsm ? "esm" : "cjs"],
       splitting: false,
       sourcemap: true,
