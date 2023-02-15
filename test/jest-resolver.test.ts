8ee85cf99c492ee6c2c0500b340369124ee52662
Arda TANRIKULU <ardatanrikulu@gmail.com>
2023-02-15 12:20:55 +0300

    Prettier makes everything prettier

--- test/jest-resolver.test.ts
+++ test/jest-resolver.test.ts
@@ -2,32 +2,32 @@ import { test, expect, it } from 'vitest';
 import jestResolver from '../jest-resolver';
 
 test('jest-resolver', () => {
-    it('removes .js extension from relative paths', () => {
-        const result = jestResolver('./foo.js', {
-            basedir: __dirname,
-            defaultResolver: path => path,
-        });
-        expect(result).toBe('./foo');
-    })
-    it('ignores absolute paths', () => {
-        const result = jestResolver('/Users/username/project/foo', {
-            basedir: __dirname,
-            defaultResolver: path => path,
-        });
-        expect(result).toBe('/Users/username/project/foo');
-    })
-    it('ignores relative paths from node_modules', () => {
-        const result = jestResolver('./foo', {
-            basedir: '/Users/username/project/node_modules/foo',
-            defaultResolver: path => path,
-        });
-        expect(result).toBe('./foo');
-    })
-    it('ignores non-js imports', () => {
-        const result = jestResolver('./foo.graphql', {
-            basedir: __dirname,
-            defaultResolver: path => path,
-        });
-        expect(result).toBe('./foo.graphql');
-    })
-})
+  it('removes .js extension from relative paths', () => {
+    const result = jestResolver('./foo.js', {
+      basedir: __dirname,
+      defaultResolver: path => path,
+    });
+    expect(result).toBe('./foo');
+  });
+  it('ignores absolute paths', () => {
+    const result = jestResolver('/Users/username/project/foo', {
+      basedir: __dirname,
+      defaultResolver: path => path,
+    });
+    expect(result).toBe('/Users/username/project/foo');
+  });
+  it('ignores relative paths from node_modules', () => {
+    const result = jestResolver('./foo', {
+      basedir: '/Users/username/project/node_modules/foo',
+      defaultResolver: path => path,
+    });
+    expect(result).toBe('./foo');
+  });
+  it('ignores non-js imports', () => {
+    const result = jestResolver('./foo.graphql', {
+      basedir: __dirname,
+      defaultResolver: path => path,
+    });
+    expect(result).toBe('./foo.graphql');
+  });
+});
